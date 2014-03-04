e3e90dc4698bbc261d82c1e3e26d162f49703a42
Steve Kingsland <steve.kingsland@opower.com>
2014-03-03 21:20:35 -0500

    Fixed the "Firebase.update failed" JS $bind() error! Login and Register are working now.

--- app/js/controllers/headerController.js
+++ app/js/controllers/headerController.js
@@ -2,18 +2,14 @@
 
 // add this controller to the existing controllers module
 angular.module('myApp.controllers').controller('HeaderController',
-           ['$rootScope', '$scope', '$location', 'syncData', 'loginService', 'waitForAuth',
-    function($rootScope,   $scope,   $location,   syncData,   loginService,   waitForAuth) {
+           ['$rootScope', '$scope', '$location', 'syncData', 'loginService',
+    function($rootScope,   $scope,   $location,   syncData,   loginService) {
         // bind the user's data to the scope, once they're logged in
 
         $scope.user = {};
 
-        // these didn't exactly work, but they might be needed to ensure we don't bind a user without being logged in
-//        waitForAuth.then(function() {
-//        if (!!($rootScope.auth && $rootScope.auth.user)) {
-
         $rootScope.$on('$firebaseSimpleLogin:login', function() {
-            syncData(['users', $rootScope.auth.user.uid]).$bind($scope, 'user').then(function(unBind) {
+            syncData(['users', $scope.auth.user.uid]).$bind($scope, 'user').then(function(unBind) {
                 $scope.unBindAccount = unBind;
             });
         });
@@ -22,20 +18,20 @@ angular.module('myApp.controllers').controller('HeaderController',
         $scope.logout = function() {
             loginService.logout();
 
-            // remove the old user's firebase and scope bindings
+            // clear the old user's firebase and scope bindings
             $scope.unBindAccount();
-            $scope.user = null;
+            $scope.user = {};
         };
 
         $scope.navbarEntries = [
-        {
-          "title": "Pools",
-          "link": "/pools"
-        },
-        {
-          "title": "Brackets",
-          "link": "/brackets"
-        }
+//        {
+//          "title": "Pools",
+//          "link": "/pools"
+//        },
+//        {
+//          "title": "Brackets",
+//          "link": "/brackets"
+//        }
         ];
 
         $scope.$on('$routeChangeSuccess', function() {
