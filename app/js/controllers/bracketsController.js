cee359990eee65b27672b6b8508a2777dfb44b9f
Steve Kingsland <steve.kingsland@opower.com>
2014-03-12 01:10:54 -0400

    Create bracket screen is done, and bracket saving to firebase is almost done.

--- app/js/controllers/bracketsController.js
+++ app/js/controllers/bracketsController.js
@@ -1,30 +1,24 @@
 'use strict';
 
 angular.module('myApp.controllers').controller('BracketsController',
-           ['$scope', '$routeParams', '$location', 'bracketService', 'teamService', 'syncData',
-    function($scope,   $routeParams,   $location,   bracketService,   teamService,   syncData) {
-
-//        function mockData() {
-//            return [{full_name: 'Arizona', seed: '1'}, {full_name: 'Duke', seed: '2'}];
-//        }
+           ['$scope', '$routeParams', '$location', 'bracketService', 'teamService', 'syncData', 'NUMBER_OF_TEAMS_PER_BRACKET', 'SUM_OF_TEAM_SEEDS_PER_BRACKET',
+    function($scope,   $routeParams,   $location,   bracketService,   teamService,   syncData,   NUMBER_OF_TEAMS_PER_BRACKET,   SUM_OF_TEAM_SEEDS_PER_BRACKET) {
+        $scope.requiredNumTeams = NUMBER_OF_TEAMS_PER_BRACKET;
+        $scope.requiredSumOfSeeds = SUM_OF_TEAM_SEEDS_PER_BRACKET;
+        $scope.sumOfSeeds = 0;
 
         $scope.poolId = $routeParams.poolId;
         $scope.bracketId = $routeParams.bracketId;
-
-        // TODO: do we need either of these?
-//      $scope.noBracket = !$routeParams.bracketId;
-//        $scope.pool = {};
 
         $scope.selectedTeams = [];
         $scope.gridOptions = {
             data: 'teams',
             selectedItems: $scope.selectedTeams,
             columnDefs: [{field:'seed', displayName:'Seed', width:60},
-                         {field:'full_name', displayName:'Name'}]
+                         {field:'full_name', displayName:'Name'}],
+            sortInfo: {fields: ['seed'], directions: ['asc']}
 //            plugins: [new ngGridFlexibleHeightPlugin()]
         };
-
-
         $scope.findTeams = function () {
             return teamService.findAll().$on('value', function(teamsSnapshot) {
                 var individualTeams = teamsSnapshot.snapshot.value;
@@ -32,17 +26,18 @@ angular.module('myApp.controllers').controller('BracketsController',
                 $scope.teams = Object.keys(individualTeams).map(function (key) {
                     return individualTeams[key];
                 });
-
-                // do I need to call this?
-//                $scope.$apply();
             });
         };
-        $scope.sumOfSeeds = function () {
+        $scope.$watchCollection('selectedTeams', function(selectedTeamsNewValue) {
+            $scope.sumOfSeeds = calculateSumOfSeeds(selectedTeamsNewValue);
+            // TODO: provide other convenience status variables for the view to use?
+        });
+        function calculateSumOfSeeds(selectedTeams) {
             // add up the seeds of all the selected teams
-            return $scope.selectedTeams.reduce(function(accum, currentValue) {
+            return selectedTeams.reduce(function(accum, currentValue) {
                 return accum + parseInt(currentValue.seed, 10);
             }, 0);
-        };
+        }
 
         $scope.findBrackets = function () {
             $scope.brackets = bracketService.findAll();
@@ -57,8 +52,12 @@ angular.module('myApp.controllers').controller('BracketsController',
             }
         };
         $scope.createBracket = function () {
-            var bracketId = bracketService.create($scope.bracket, $scope.poolId, $scope.auth.user.uid, function (err) {
-                if(!err) {
+            console.log('createBracket called, $scope.selectedTeams =', $scope.selectedTeams);
+
+            // TODO: create an array of all the $scope.selectedTeams.id values, and set as $scope.bracket.teams
+
+            bracketService.create($scope.bracket, $scope.poolId, $scope.auth.user.uid).then(function (ref) {
+                if(!!ref) {
                     $scope.bracket = null;
                     $location.path('/pools/' + poolId + '/brackets/' + bracketId);
                     $scope.$apply();
--- app/js/services/bracketService.js
+++ app/js/services/bracketService.js
@@ -16,7 +16,7 @@ angular.module('myApp.services').service('bracketService',
 
         this.create = function(bracket, poolId, ownerId) {
              // add the bracket primarily to the list of brackets
-            $firebase(bracketsRef)
+            return $firebase(bracketsRef)
                 .$add({name: bracket.name, teams: bracket.teams, poolId: poolId, ownerId: ownerId})
                 .then(function (ref) {
                     // TODO: is this right? Or should it be ref.$id?
@@ -26,7 +26,6 @@ angular.module('myApp.services').service('bracketService',
                     $firebase(poolsRef).$child(poolId).$child('brackets').$add(bracketId);
                     $firebase(usersRef).$child(ownerId).$child('brackets').$add(bracketId);
                 });
-
         };
 
         /*
