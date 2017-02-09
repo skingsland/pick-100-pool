f98bf4ff14d2c8e3fa5af7db00db274c8e472b2a
Steve Kingsland <steve.kingsland@opower.com>
2017-02-08 21:12:34 -0800

    delete ui-grid bower files, since it's manually downloaded instead

--- client/vendor/ui-grid/grunt/bump.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  options: {
-    files: ['package.json', 'bower.json'],
-    commitFiles: ['package.json', 'bower.json', 'CHANGELOG.md'],
-    push: false,
-    commitMessage: 'chore: Release v%VERSION%'
-  }
-};
