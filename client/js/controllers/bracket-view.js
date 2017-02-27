3453729e6c3041b6736c82677219d39b096eb7e8
Steve Kingsland <steve.kingsland@opower.com>
2017-02-26 21:19:10 -0800

    rewrites bracket view ngGrid table with SmartTable, which is much faster
    The styling is a tiny bit different (mostly on purpose), but barely perceptible, and maybe an improvement.

--- client/js/controllers/bracket-view.js
+++ client/js/controllers/bracket-view.js
@@ -1,8 +1,8 @@
 'use strict';
 
 angular.module('myApp.controllers').controller('ViewBracketController',
-           ['$scope', '$routeParams', '$location', '$q', '$timeout', 'poolService', 'bracketService', 'teamService', 'userService', 'NUMBER_OF_TEAMS_PER_BRACKET', 'SUM_OF_TEAM_SEEDS_PER_BRACKET',
-    function($scope,   $routeParams,   $location,   $q,   $timeout,   poolService,   bracketService,   teamService,   userService,   NUMBER_OF_TEAMS_PER_BRACKET,   SUM_OF_TEAM_SEEDS_PER_BRACKET) {
+           ['$scope', '$routeParams', '$q', 'poolService', 'bracketService', 'teamService', 'userService', 'SUM_OF_TEAM_SEEDS_PER_BRACKET',
+    function($scope,   $routeParams,   $q,   poolService,   bracketService,   teamService,   userService,   SUM_OF_TEAM_SEEDS_PER_BRACKET) {
         $scope.sumOfSeeds = SUM_OF_TEAM_SEEDS_PER_BRACKET;
         $scope.poolId = $routeParams.poolId;
 
@@ -19,24 +19,6 @@ angular.module('myApp.controllers').controller('ViewBracketController',
 
         // the bracketId can be supplied in two different ways: from the parent scope, or via a route param (e.g. /bracket/1)
         $scope.bracketId = $scope.bracketId || $routeParams.bracketId;
-
-        $scope.bracketGridOptions = {
-            data: 'teamsWithScores',
-            enableRowSelection: false,
-            headerRowHeight: 50, // allow room for the <br/>
-            rowHeight: 25,
-            rowTemplate:
-                "<div ng-style=\"{ 'cursor': row.cursor }\" ng-repeat=\"col in renderedColumns\" ng-class=\"[col.colIndex(), row.getProperty('is_eliminated') ? 'eliminated' : '']\" class=\"ngCell {{col.cellClass}}\">\n" +
-                "\t<div class=\"ngVerticalBar\" ng-style=\"{height: rowHeight}\" ng-class=\"{ ngVerticalBarVisible: !$last }\">&nbsp;</div>\n" +
-                "\t<div ng-cell></div>\n" +
-                "</div>",
-            columnDefs: buildColumnDefsForBracketGrid(),
-            showFooter: true,
-            footerRowHeight: 30,
-            footerTemplate: buildFooterTemplateForBracketGrid(),
-            // TODO fix sorting (this doesn't seem to have any effect, perhaps because the data is loaded after the grid renders)
-            sortInfo: {fields: ['seed'], directions: ['asc']}
-        };
 
         $scope.removeBracket = function() {
             bracketService.removeBracket($scope.bracketId);
@@ -118,51 +100,6 @@ angular.module('myApp.controllers').controller('ViewBracketController',
 
                 return previousValue + currentValue;
             }, 0);
-        }
-
-        function buildColumnDefsForBracketGrid() {
-            // ngGrid's minWidth property doesn't work, so set the exact width for the first columns
-            var columnDefs = [
-                {field:'full_name', displayName:'Picks', width:150},
-                {field:'seed', displayName:'Seed', width:60}
-            ];
-
-            for (var i = 1; i <= 6; i++) {
-                var points = Math.pow(2, i - 1);
-
-                columnDefs.push({field: 'rounds[' + i + ']',
-                                 displayName: 'Round ' + i + '<br/><small>Seed # + ' + points + ' point' + (points > 1 ? 's' : '') + '</small>',
-                                 cellTemplate: '<div class="ngCellText" ng-class="col.colIndex()">'
-                                                + '<span ng-cell-text>{{COL_FIELD || \'\'}}</span>' // don't show zeros; it's distracting
-                                             + '</div>'
-                });
-            }
-
-            columnDefs.push({field:'totalPoints', displayName:'Team Total'});
-            return columnDefs;
-        }
-
-        function buildFooterTemplateForBracketGrid() {
-            var footerTemplate = '<div class="ngFooterPanel" ng-style="footerStyle()">'
-                + '                   <div class="ngFooterCell col0 colt0">'
-                + '                       <span class="ngLabel">TOTALS:</span>'
-                + '                   </div>'
-                + '                   <div class="ngFooterCell col1 colt1">'
-                + '                       <span class="ngLabel">{{sumOfSeeds}}</span>'
-                + '                   </div>';
-
-            for (var i = 1; i <= 6; i++) {
-                footerTemplate += '   <div class="ngFooterCell col' + (i+1) + ' colt' + (i+1) + '">'
-                + '                       <span class="ngLabel">{{bracket.total_bracket_points_for_round[' + i + '] || \'\'}}</span>'
-                + '                   </div>';
-            }
-
-            footerTemplate += '       <div class="ngFooterCell col7 colt7">'
-            + '                           <span class="ngLabel">{{sumOfPoints}}</span>'
-            + '                       </div>'
-            + '                   </div>';
-
-            return footerTemplate;
         }
     }
 ]);
